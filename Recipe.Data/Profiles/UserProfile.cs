79a611c521b35cf448355e5223798d5e8e0a4ae0
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-11-28 16:37:52 +0200

    Added add user service and profile

---
+++ Recipe.Data/Profiles/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Recipe.Data.Models.Domains;
+using Recipe.Data.Models.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterUserDTO, User>()
+                .ForMember(user => user.CreatedAt, map => map.MapFrom(_ => DateTime.Now))
+                .ForMember(user => user.Status, map => map.MapFrom(_ => "Active"))
+                .ForMember(user => user.Role, map => map.MapFrom(_ => "User"));
+
+        }
+    }
+}
---
+++ Recipe.Data/Services/UserServices.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Recipe.Data.Models.Domains;
+using Recipe.Data.Models.DTOs.User;
+using Recipe.Data.Repository.Implementation;
+using Recipe.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data.Services
+{
+    public class UserServices(IRepository<User> _userRepository, IMapper _mapper)
+    {
+        public async Task AddUserAsync(RegisterUserDTO request)
+        {
+            var user = _mapper.Map<User>(request);
+            await _userRepository.AddAsync(user);
+        }
+    }
+}
