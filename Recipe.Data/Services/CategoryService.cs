4210ef8abf34892f7bf28521436aa1bfe5d8413a
Tshiamo Justice Mothokho <119753025+TJ-Mothokho@users.noreply.github.com>
2024-11-29 21:28:24 +0200

    Update CategoryService.cs

--- Recipe.Data/Services/CategoryService.cs
+++ Recipe.Data/Services/CategoryService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Recipe.Data.Services
 {
-    internal class CategoryService
+    public class CategoryService(ICategoryRepository _category, IMapper _mapper)
     {
+        public async Task<bool> AddCategory(CreateCategoryDTO request)
+        {
+            try
+            {
+                var category = _mapper.
+                var response = _category.AddAsync(category);
+                return response;
+            }
+            catch{return false;}
+        }
+
+        public async Task<IEnumerable<ReadCategoryDTO>> GetAllCategories()
+        {
+            
+        }
     }
 }
