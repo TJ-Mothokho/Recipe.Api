e3887f55ce401378b7fc04150930fabbeda3a186
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-11-29 06:27:48 +0200

    Added Category repos

---
+++ Recipe.Data/Repository/Implementation/CategoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipe.Data.Context;
+using Recipe.Data.Models.Domains;
+using Recipe.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data.Repository.Implementation
+{
+    public class CategoryRepository(ApplicationDbContext _context) : ICategoryRepository
+    {
+        public async Task<bool> AddCategory(Category category)
+        {
+            try
+            {
+                await _context.Categories.AddAsync(category);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public async Task<bool> DeleteCategory(Guid id)
+        {
+            try
+            {
+                var category = await GetCategoryById(id);
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public async Task<IEnumerable<Category>> GetAllCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+        public async Task<Category> GetCategoryById(Guid id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+        public async Task<bool> UpdateCategory(Category category)
+        {
+            try
+            {
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Recipe.Data/Repository/Interface/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Recipe.Data.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data.Repository.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(Guid id);
+        Task<bool> AddCategory(Category category);
+        Task<bool> UpdateCategory(Category category);
+        Task<bool> DeleteCategory(Guid id);
+    }
+}
