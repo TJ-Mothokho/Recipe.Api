3488f0f22c0dd319aaa4eb0585976e4f3a59a93a
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-11-29 19:14:54 +0200

    Added Service and Recipe Controller

---
+++ Recipe.Api/Controllers/RecipesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Data.Models.DTOs.Recipe;
+using Recipe.Data.Services;
+
+namespace Recipe.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController(RecipeService _recipeService) : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAllRecipes()
+        {
+            var recipes = await _recipeService.GetAllRecipesAsync();
+            return Ok(recipes);
+        }
+        [HttpGet("/Get/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var recipe = await _recipeService.GetRecipeByIdAsync(id);
+            return Ok(recipe);
+        }
+        [HttpGet("category/{categoryID}")]
+        public async Task<IActionResult> GetByCategoryAsync(Guid categoryID)
+        {
+            var recipes = await _recipeService.GetByCategoryAsync(categoryID);
+            return Ok(recipes);
+        }
+        [HttpGet("hashtag/{hashtagID}")]
+        public async Task<IActionResult> GetByHashtagAsync(Guid hashtagID)
+        {
+            var recipes = await _recipeService.GetByHashtagAsync(hashtagID);
+            return Ok(recipes);
+        }
+        [HttpGet("user/{userID}")]
+        public async Task<IActionResult> GetByUserAsync(Guid userID)
+        {
+            var recipes = await _recipeService.GetByUserAsync(userID);
+            return Ok(recipes);
+        }
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddRecipeAsync(CreateRecipeDTO request)
+        {
+            var result = await _recipeService.AddRecipeAsync(request);
+            return Ok(result);
+        }
+
+        [HttpPut("/Update/{id}")]
+        public async Task<IActionResult> UpdateRecipeAsync(UpdateRecipeDTO request)
+        {
+            var result = await _recipeService.UpdateRecipeAsync(request);
+            return Ok(result);
+        }
+
+        [HttpDelete("/Delete/{id}")]
+        public async Task<IActionResult> DeleteRecipeAsync(Guid id)
+        {
+            var result = await _recipeService.DeleteRecipeAsync(id);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Recipe.Data/Services/RecipeService.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Recipe.Data.Models.Domains;
+using Recipe.Data.Models.DTOs.Recipe;
+using Recipe.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data.Services
+{
+    public class RecipeService(IRecipeRepository _recipeRepository, IMapper _mapper)
+    {
+        public async Task<bool> AddRecipeAsync(CreateRecipeDTO request)
+        {
+            try
+            {
+                var recipe = _mapper.Map<RecipeModel>(request);
+                await _recipeRepository.AddRecipe(recipe);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteRecipeAsync(Guid id)
+        {
+            try
+            {
+                await _recipeRepository.DeleteRecipe(id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<RecipeDetailsDTO>> GetAllRecipesAsync()
+        {
+            var recipes = await _recipeRepository.GetAllRecipes();
+            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            return response;
+        }
+
+        public async Task<IEnumerable<RecipeDetailsDTO>> GetByCategoryAsync(Guid categoryID)
+        {
+            var recipes = await _recipeRepository.GetByCategory(categoryID);
+            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            return response;
+        }
+
+        public async Task<IEnumerable<RecipeDetailsDTO>> GetByHashtagAsync(Guid hashtagID)
+        {
+            var recipes = await _recipeRepository.GetByHashtag(hashtagID);
+            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            return response;
+        }
+
+        public async Task<IEnumerable<RecipeDetailsDTO>> GetByUserAsync(Guid userID)
+        {
+            var recipes = await _recipeRepository.GetByUser(userID);
+            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            return response;
+        }
+
+        public async Task<RecipeDetailsDTO> GetRecipeByIdAsync(Guid id)
+        {
+            var recipe = await _recipeRepository.GetRecipeById(id);
+            var response = _mapper.Map<RecipeDetailsDTO>(recipe);
+            return response;
+        }
+
+        public async Task<bool> UpdateRecipeAsync(UpdateRecipeDTO request)
+        {
+            try
+            {
+                var recipe = _mapper.Map<RecipeModel>(request);
+                await _recipeRepository.UpdateRecipe(recipe);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
