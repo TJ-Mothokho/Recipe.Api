b50538da9c6a531769d507c28a5a57ef3164f511
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-12-07 06:54:38 +0200

    Fixed 1 of 2bugs

--- Recipe.Data/Services/RecipeService.cs
+++ Recipe.Data/Services/RecipeService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Azure;
 using Recipe.Data.Models.Domains;
 using Recipe.Data.Models.DTOs.Recipe;
 using Recipe.Data.Repository.Interface;
@@ -56,35 +57,46 @@ namespace Recipe.Data.Services
         public async Task<IEnumerable<RecipeDetailsDTO>> GetAllRecipesAsync()
         {
             var recipes = await _recipeRepository.GetAllRecipes();
-            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            var map = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            var response = await AddRecipeDetails(map);
+
             return response;
         }
 
         public async Task<IEnumerable<RecipeDetailsDTO>> GetByCategoryAsync(Guid categoryID)
         {
             var recipes = await _recipeRepository.GetByCategory(categoryID);
-            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            var map = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+
+            var response = await AddRecipeDetails(map);
             return response;
         }
 
         public async Task<IEnumerable<RecipeDetailsDTO>> GetByHashtagAsync(Guid hashtagID)
         {
             var recipes = await _recipeRepository.GetByHashtag(hashtagID);
-            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            var map = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+
+            var response = await AddRecipeDetails(map);
             return response;
         }
 
         public async Task<IEnumerable<RecipeDetailsDTO>> GetByUserAsync(Guid userID)
         {
             var recipes = await _recipeRepository.GetByUser(userID);
-            var response = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+            var map = _mapper.Map<IEnumerable<RecipeDetailsDTO>>(recipes);
+
+            var response = await AddRecipeDetails(map);
             return response;
         }
 
         public async Task<RecipeDetailsDTO> GetRecipeByIdAsync(Guid id)
         {
             var recipe = await _recipeRepository.GetRecipeById(id);
-            var response = _mapper.Map<RecipeDetailsDTO>(recipe);
+            var map = _mapper.Map<RecipeDetailsDTO>(recipe);
+
+            var response = await AddRecipeDetails(map);
+
             return response;
         }
 
@@ -92,7 +104,19 @@ namespace Recipe.Data.Services
         {
             try
             {
-                var recipe = _mapper.Map<RecipeModel>(request);
+                //var recipe = _mapper.Map<RecipeModel>(request);
+                var recipe = new RecipeModel
+                {
+                    
+                    RecipeID = request.RecipeID,
+                    Title = request.Title,
+                    Instructions = request.Instructions,
+                    ImageUrl = request.ImageUrl,
+                    CategoryID = request.CategoryID,
+                    IsEdited = true
+
+                    //Hashtags = request.Hashtags.Select(x => new Hashtag { Name = x }).ToList()
+                };
                 await _recipeRepository.UpdateRecipe(recipe);
                 return true;
             }
@@ -101,5 +125,30 @@ namespace Recipe.Data.Services
                 return false;
             }
         }
+
+        private async Task<IEnumerable<RecipeDetailsDTO>> AddRecipeDetails(IEnumerable<RecipeDetailsDTO> recipes)
+        {
+            foreach (var item in recipes)
+            {
+                var category = await _category.GetCategotyById(item.CategoryID);
+                item.Category = category.Name;
+
+                var user = await _user.GetUserByIdAsync(item.UserID);
+                item.UserName = user.Username;
+            }
+            return recipes;
+        }
+
+        private async Task<RecipeDetailsDTO> AddRecipeDetails(RecipeDetailsDTO recipes)
+        {
+            var category = await _category.GetCategotyById(recipes.CategoryID);
+            recipes.Category = category.Name;
+
+            var user = await _user.GetUserByIdAsync(recipes.UserID);
+            recipes.UserName = user.Username;
+
+            return recipes;
+        }
+
     }
 }
